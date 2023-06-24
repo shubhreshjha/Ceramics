996839181c28eb1c1f0155db20b1d0a32a315fbd
Shubhresh Kumar Jha <119638091+shubhreshjha@users.noreply.github.com>
2023-06-24 00:53:32 -0500

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -2,9 +2,12 @@
 <!DOCTYPE html>
 <html>
 <body>
+document.addEventListener("DOMContentLoaded", function()
 function addToCart(itemName, itemPrice) {
   var cartItems = document.getElementById("cart-items");
   var cartTotal = document.getElementById("cart-total");
+  var checkoutButton = document.querySelector(".checkout-button");
+
 
   // Create a new list item for the cart
   var listItem = document.createElement("li");
@@ -14,8 +17,18 @@ function addToCart(itemName, itemPrice) {
   // Update the total price
   var currentTotal = parseFloat(cartTotal.textContent);
   cartTotal.textContent = (currentTotal + itemPrice).toFixed(2);
-}
 
+checkoutButton.addEventListener("click", function() {
+    // Handle the checkout logic here
+    var total = parseFloat(cartTotal.textContent);
+    alert("Thank you for your purchase! Total amount: $" + total.toFixed(2));
+    // Reset the cart
+    cartItems.innerHTML = "";
+    cartTotal.textContent = "0.00";
+
+});
+
+ });
 
  
 </body>
