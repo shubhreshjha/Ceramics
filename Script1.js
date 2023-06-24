3cb1ff758c4427fc14d1e4447a8fa564b732f1aa
Shubhresh Kumar Jha <119638091+shubhreshjha@users.noreply.github.com>
2023-06-24 00:30:34 -0500

    Update Script1.js
    Shopping Cart for Featured Pottery

--- Script1.js
+++ Script1.js
@@ -2,6 +2,19 @@
 <!DOCTYPE html>
 <html>
 <body>
+function addToCart(itemName, itemPrice) {
+  var cartItems = document.getElementById("cart-items");
+  var cartTotal = document.getElementById("cart-total");
+
+  // Create a new list item for the cart
+  var listItem = document.createElement("li");
+  listItem.textContent = itemName + " - $" + itemPrice.toFixed(2);
+  cartItems.appendChild(listItem);
+
+  // Update the total price
+  var currentTotal = parseFloat(cartTotal.textContent);
+  cartTotal.textContent = (currentTotal + itemPrice).toFixed(2);
+}
 
 
  
